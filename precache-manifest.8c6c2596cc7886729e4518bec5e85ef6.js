17b9218565e8874e2e8bb8d28da5108ddd90eaac
yskim <spro1@naver.com>
2020-06-14 06:47:55 +0900

    Updates

---
+++ precache-manifest.8c6c2596cc7886729e4518bec5e85ef6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6968b747780491d689a943906d061058",
+    "url": "/inha_project/index.html"
+  },
+  {
+    "revision": "7c79e7bfb0c70d7294cc",
+    "url": "/inha_project/static/css/2.1d1a23ac.chunk.css"
+  },
+  {
+    "revision": "035bda2ff5b2fa2b6748",
+    "url": "/inha_project/static/css/main.dc33f2d5.chunk.css"
+  },
+  {
+    "revision": "7c79e7bfb0c70d7294cc",
+    "url": "/inha_project/static/js/2.f0da482b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/inha_project/static/js/2.f0da482b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035bda2ff5b2fa2b6748",
+    "url": "/inha_project/static/js/main.9ae150ad.chunk.js"
+  },
+  {
+    "revision": "518b325d5b015db120c3",
+    "url": "/inha_project/static/js/runtime-main.c914b28f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inha_project/static/media/logo.5d5d9eef.svg"
+  }
+]);
