120731a592ab9fd37a9f32f1e7b0ef0e01a0e23b
yskim <spro1@naver.com>
2020-06-15 13:54:15 +0900

    add data

--- src/components/StateBoard.js
+++ src/components/StateBoard.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import Badge from "react-bootstrap/esm/Badge";
 import Logo from "../img/Logo.png";
 import State from "../img/State.png";
 import Confirm from "../img/confirm.png";
@@ -18,6 +19,19 @@ export default class Main extends Component{
         super(props);
     }
     render() {
+        let inc_confirm, inc_cure, inc_iso, inc_death = 0;
+        inc_confirm = ((this.props.KoreaData.new_confirm/(this.props.KoreaData.confirm)) * 100).toFixed(2);
+        inc_cure = ((this.props.KoreaData.new_cure/(this.props.KoreaData.cure)) * 100).toFixed(2);
+        inc_iso = ((this.props.KoreaData.new_isolation/(this.props.KoreaData.isolation)) * 100).toFixed(2);
+        inc_death = ((this.props.KoreaData.new_death/(this.props.KoreaData.death)) * 100).toFixed(2);
+
+        var _Rate = null;
+         if (inc_death > 0) {
+             _Rate = <Badge variant="danger">{inc_death}% increase</Badge>;
+         } else  if(inc_death <= 0){
+             _Rate = <Badge variant="success">{inc_death}% increase</Badge>;
+         }
+
         this.props.KoreaData.confirm = numberFormat(this.props.KoreaData.confirm);
         this.props.KoreaData.isolation = numberFormat(this.props.KoreaData.isolation);
         this.props.KoreaData.cure = numberFormat(this.props.KoreaData.cure);
@@ -41,14 +55,22 @@ export default class Main extends Component{
                         <div className="corona-box-wrap">
                             <span className="corona-icon"><img src={Confirm}/></span>
                             <h6 className="font-gray">확진</h6>
-                            <h1 className="font-red">{this.props.KoreaData.confirm}</h1>
+                            <h1>{this.props.KoreaData.confirm}</h1>
+                            <div className="badge-box">
+                                <Badge variant="danger">{inc_confirm}% increase</Badge>
+                                <h6 className="badge-desc">+{this.props.KoreaData.new_confirm}</h6>
+                            </div>
                         </div>
                     </Col>
                     <Col md={6} className="corona-box">
                         <div className="corona-box-wrap">
                             <span className="corona-icon"><img src={Cure}/></span>
                             <h6 className="font-gray">완치</h6>
-                            <h1 className="font-green">{this.props.KoreaData.cure}</h1>
+                            <h1>{this.props.KoreaData.cure}</h1>
+                            <div className="badge-box">
+                                <Badge variant="success">{inc_cure}% increase</Badge>
+                                <h6 className="badge-desc">+{this.props.KoreaData.new_cure}</h6>
+                            </div>
                         </div>
                     </Col>
                     <Col md={6} className="corona-box">
@@ -56,13 +78,21 @@ export default class Main extends Component{
                             <span className="corona-icon"><img src={Isolation}/></span>
                             <h6 className="font-gray">격리</h6>
                             <h1>{this.props.KoreaData.isolation}</h1>
+                            <div className="badge-box">
+                                <Badge variant="danger">{inc_iso}% increase</Badge>
+                                <h6 className="badge-desc">+{this.props.KoreaData.new_isolation}</h6>
+                            </div>
                         </div>
                     </Col>
                     <Col md={6} className="corona-box">
                         <div className="corona-box-wrap">
                             <span className="corona-icon"><img src={Death}/></span>
                             <h6 className="font-gray">사망</h6>
-                            <h1 className="font-red">{this.props.KoreaData.death}</h1>
+                            <h1>{this.props.KoreaData.death}</h1>
+                            <div className="badge-box">
+                                {_Rate}
+                                <h6 className="badge-desc">+{this.props.KoreaData.new_death}</h6>
+                            </div>
                         </div>
                     </Col>
 
