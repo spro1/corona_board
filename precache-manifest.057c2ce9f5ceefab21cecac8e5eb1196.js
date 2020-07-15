3dc085a1147df7439b7cbe64c7cb91cfa32a07de
yskim <spro1@naver.com>
2020-07-15 11:11:27 +0900

    Updates

--- precache-manifest.057c2ce9f5ceefab21cecac8e5eb1196.js
+++ precache-manifest.057c2ce9f5ceefab21cecac8e5eb1196.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "63c8172f4d3d088aa9e14e1c4668d28f",
+    "revision": "34e0cf5cad54d283cfca6fc38f88da5a",
     "url": "/inha_project/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/inha_project/static/css/2.1d1a23ac.chunk.css"
   },
   {
-    "revision": "0b29b2a3aa0d8798e1c8",
+    "revision": "a3bee33a301653d6d53a",
     "url": "/inha_project/static/css/main.f025f512.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/inha_project/static/js/2.9b95f5f7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0b29b2a3aa0d8798e1c8",
-    "url": "/inha_project/static/js/main.7ae498e6.chunk.js"
+    "revision": "a3bee33a301653d6d53a",
+    "url": "/inha_project/static/js/main.7a041074.chunk.js"
   },
   {
     "revision": "518b325d5b015db120c3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/inha_project/precache-manifest.ae7955baf50e7ef505fa260c5eb1b395.js"
+  "/inha_project/precache-manifest.057c2ce9f5ceefab21cecac8e5eb1196.js"
 );
 
 self.addEventListener('message', (event) => {
